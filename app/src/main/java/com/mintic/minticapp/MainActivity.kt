dae8983bd3427184153ebb1d02f7cfb307f61b93
GPFrederick <gpf95@protonmail.com>
2021-11-16 17:44:00 -0500

    Agregar Toolbar personalizada, navegación entre Activities y String Resources

--- app/src/main/java/com/mintic/minticapp/MainActivity.kt
+++ app/src/main/java/com/mintic/minticapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.mintic.minticapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.especificacion)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.base_toolbar))
+
+        val viewButton = findViewById<Button>(R.id.ver1Button)
+        viewButton.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
