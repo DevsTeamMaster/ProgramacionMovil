fec94f9f0ab6ab07547384b3eda8d68b516d8811
GPFrederick <gpf95@protonmail.com>
2021-12-12 12:56:59 -0500

    Archivos innecesarios eliminados

--- app/src/main/java/com/mintic/minticapp/ListOfPlaces.kt
+++
@@ -1,9 +0,0 @@
-package com.mintic.minticapp
-
-data class ListOfPlaces(
-    val name: String,
-    val description: String,
-    val imageURL: String,
-    val punctuation: String,
-    val temperature: String
-)
